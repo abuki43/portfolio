faadfe8bb6510fa97f1b899d28e5d59a0eec6bab
abuki43 <abukiboss@gmail.com>
2026-03-22 18:16:06 +0300

    minor update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -50,7 +50,7 @@ const Projects = () => {
     },{
       title:"ExitExamStudio",
       image:ExitLogo,
-      description:"Exit Exam studio is web app that provides past exit exam questions for Ethiopian universities students. It allows students to practice for their exams. Over 1200+ students have used the app to prepare for their exit exams.",
+      description:"Exit Exam studio is web app that provides past exit exam questions for Ethiopian universities students. It allows students to practice for their exams. Over 3500+ students have used the app to prepare for their exit exams.",
       technologies:["Nextjs"],
       demo:"exitexamstudio.app",
       github:"",
